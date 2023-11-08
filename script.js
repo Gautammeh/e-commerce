6751d1104e9f50168b1dc76bc126606df758e645
Gautammeh <gautammehta092@gmail.com>
2023-11-08 17:32:37 +0530

    chnges in all 3

---
+++ script.js
@@ -0,0 +1,29 @@
+const hamburger = document.getElementById("hamburger");
+const dropdown = document.getElementById("dropdown");
+
+let val = 0
+hamburger.addEventListener("click",()=>{
+
+    if(val== 0){
+
+        
+    
+    dropdown.style.display = "block";
+    hamburger.removeAttribute;
+    hamburger.setAttribute("class","ri-close-fill");
+    val = 1;
+
+
+    }
+    else{
+    dropdown.style.display = "none";
+    hamburger.removeAttribute;
+    hamburger.setAttribute("class","ri-menu-line");
+    val = 0;
+
+    }
+    
+
+
+
+})
